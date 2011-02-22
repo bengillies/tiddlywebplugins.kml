4f25e4278d996d53ec90b2ba237543c68315f622
Ben Gillies <bengillies@gmail.com>
2011-02-22 17:24:25 +0000

    added an init function and a blank Serialization class
    The init function registers the serializaation class with tiddlyweb

--- tiddlywebplugins/kml.py
+++ tiddlywebplugins/kml.py
@@ -1,4 +1,16 @@
 """
 A serializer for kml files
 """
+from tiddlyweb.serializations import SerializationInterface
 
+class Serialization(SerializationInterface):
+    pass
+
+def init(config):
+    """
+    add the serialization for content type application/vnd.google-earth.kml+xml
+    """
+    mime_type = 'application/vnd.google-earth.kml+xml'
+    config['extension_types']['kml'] = mime_type
+    config['serializers'][mime_type] = ['tiddlywebplugins.kml',
+        '%s; charset=UTF8' % mime_type]
