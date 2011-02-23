ee28a00df486f0fbb3b06b25b9083621b0f912e9
Ben Gillies <bengillies@gmail.com>
2011-02-23 16:16:00 +0000

    Added code to serialize tiddlers into kml
    tiddler_as and list_tiddlers written. as_tiddler still to be done.
    
    Tiddlers without any geo data will currently return an empty kml file.

--- tiddlywebplugins/kml.py
+++ tiddlywebplugins/kml.py
@@ -3,8 +3,67 @@ A serializer for kml files
 """
 from tiddlyweb.serializations import SerializationInterface
 
+from pykml.factory import KML_ElementMaker as KML
+from pykml.factory import ATOM_ElementMaker as ATOM
+from lxml import etree
+
 class Serialization(SerializationInterface):
-    pass
+    """
+    This is the meat of the plugin. It provides 3 functions that enable it to
+    serialize and deserialize tiddlers, and serialize lists of tiddlers (aka
+    bags).
+    """
+    def tiddler_as(self, tiddler):
+        """
+        serialize a tiddler into kml format
+        """
+        kml = self.tiddler_to_kml(tiddler)
+        return self.kml_to_string(kml)
+
+    def list_tiddlers(self, tiddlers):
+        """
+        serialize a list of tiddlers into kml format
+        """
+        kml = KML.kml()
+        for tiddler in tiddlers:
+            tiddler_kml = self.tiddler_to_kml(tiddler)
+            kml.append(tiddler_kml)
+
+        return self.kml_to_string(kml)
+
+    def as_tiddler(self, tiddler, input_string):
+        """
+        serialize a kml file into a tiddler
+        """
+        pass
+
+    def tiddler_to_kml(self, tiddler):
+        """
+        convert one tiddler into a kml representation as an etree
+        """
+        geo_long = tiddler.fields.get('geo.long')
+        geo_lat = tiddler.fields.get('geo.lat')
+        if geo_long is not None and geo_lat is not None:
+            kml = KML.kml(
+                KML.Placemark(
+                    KML.name(tiddler.title),
+                    KML.Point(
+                        KML.coordinates('%s,%s' % (geo_long, geo_lat))
+                    )
+                )
+            )
+        else:
+            kml = KML.kml()
+
+        return kml
+
+    def kml_to_string(self, kml):
+        """
+        convert an etree representation of kml to a valid xml document in
+        string format
+        """
+        return '<?xml version="1.0" encoding="UTF-8"?>%s' % etree.tostring(kml)
+
 
 def init(config):
     """
